b2338644a9cd6bcff6695269672bfab956092f6c
Jonathan318 <jonathanmartinezc0910@ciencias.unam.mx>
2023-10-28 23:44:27 -0600

    Añado fuente que vamos a utilizar

--- front-end/src/components/Principal.js
+++ front-end/src/components/Principal.js
@@ -7,10 +7,10 @@ function Principal(){
     return (
         <body>
             <div>   
-            <h1>GmG Tournaments</h1>
-            <Link to='/login' type="button" class="btn btn-outline-primary" >Iniciar Sesion</Link>
+            <h1>GmG <br></br>Tournaments</h1>
+            <Link to='/login' type="button" class="btn btn-outline-secondary" >Iniciar Sesion</Link>
             <br></br>
-            <Link to='/'>Registrar</Link>
+            <Link to='/' type="button" class="btn btn-outline-secondary" >Registrar</Link>
             </div>
         </body>
       );
